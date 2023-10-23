e362f83e5b278a792eaf7f29088b9f2883a3362f
Morbius <Morghandenham097@riversideunified.org>
2023-10-23 14:12:27 -0700

    nuh uh

--- prototype 3 sounds and effects/Assets/scripts/playermove.cs
+++ prototype 3 sounds and effects/Assets/scripts/playermove.cs
@@ -6,11 +6,15 @@ public class playermove : MonoBehaviour
 {
     public float JumpForce = 10f;
     public float Gravity = 1f;
+    public bool IsOnGround = true;
     private Rigidbody _playerRb;
+    private Animator _playeranim;
+    
     // Start is called before the first frame update
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
+        _playeranim = GetComponent<Animator>();
         //_playerRb.AddForce(Vector3.up * 1000);
         Physics.gravity *= Gravity;
     }
@@ -18,10 +22,16 @@ public class playermove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround) 
         {
             _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+            _playeranim
         }
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsOnGround = true;
     }
 }
---
+++ prototype 3 sounds and effects/Assets/scripts/spawnmanager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanager : MonoBehaviour
+{
+    public GameObject obstacle;
+    private Vector3 spawnPosition = new Vector3(25, 0,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstacle, spawnPosition, obstacle.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
