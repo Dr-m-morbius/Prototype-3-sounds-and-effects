6e842c63204062845c6dd4cdbc8f44301b5b586b
Morbius <Morghandenham097@riversideunified.org>
2023-11-07 20:38:57 -0800

    nuh uh

--- prototype 3 sounds and effects/Assets/scripts/playermove.cs
+++ prototype 3 sounds and effects/Assets/scripts/playermove.cs
@@ -10,10 +10,16 @@ public class playermove : MonoBehaviour
     public bool IsOnGround = true;
     private Rigidbody _playerRb;
     private Animator _playeranim;
-    
+    public ParticleSystem explosion;
+    public ParticleSystem dirt;
+    public AudioClip jump;
+    public AudioClip crash;
+    private AudioSource playerAudio;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         _playerRb = GetComponent<Rigidbody>();
         _playeranim = GetComponent<Animator>();
         //_playerRb.AddForce(Vector3.up * 1000);
@@ -28,7 +34,8 @@ public class playermove : MonoBehaviour
             _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             IsOnGround = false;
             _playeranim.SetTrigger("Jump_trig");
-           
+            dirt.Stop();
+            playerAudio.PlayOneShot(jump, 1.0f);
         }
 
     }
@@ -39,12 +46,16 @@ public class playermove : MonoBehaviour
         if (collision.gameObject.CompareTag("ground"))
         {
             IsOnGround = true;
+            dirt.Play();
         } else if (collision.gameObject.CompareTag("obsticle"))
         {
             gameover = true;
             Debug.Log("game over");
             _playeranim.SetBool("Death_b", true);
             _playeranim.SetInteger("DeathType_int", 1);
+            explosion.Play();
+            dirt.Stop();
+            playerAudio.PlayOneShot(crash, 1.0f);
         }
     }
 }
